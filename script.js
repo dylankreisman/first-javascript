296c069037c58afeec353d71d584388ec6554b09
Dr Dyl <dylkreisman@gmail.com>
2022-06-24 01:37:34 -0500

    made loop, now creating prompts

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // defining characters for generator
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const numbers = "0123456789";
-const symbols = "!@#$%^&*_-+=";
+ 
+var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*_-+=";
+var charNum = ""
+for (let i = 8; i < 128; i++);   
 
+ var generateConf = [alphaLower, alphaUpper, numbers, symbols] 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 document.getElementById("generate").addEventListener("click", generatePrompt);
 function generatePrompt() {
-  var generatePrompt = prompt("What would you like included in your password?")
-  alert (generatePrompt);
+  var res
+  var charCount = prompt("How many characters would you like in your password?")
+  if (charCount == null || charCount == "") {
+    res="Please try again."
+  }
   };
-  
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword("");
@@ -19,8 +29,10 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+}; 
 
+
+password = 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
